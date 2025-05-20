78980210d87196427c5908cd48756fbe6db84299
nabiraaaaaa <nabiraagiztabia@gmail.com>
2025-05-20 07:53:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🫧 tes")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start building! For help and inspiration."
 )
 st.image("IMG_4658.jpeg", width=200)
