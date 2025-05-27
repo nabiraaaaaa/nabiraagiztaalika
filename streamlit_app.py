2f81e120123ef0f78a558b833b966821b2718b19
nabiraaaaaa <nabiraagiztabia@gmail.com>
2025-05-27 07:48:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,14 @@ st.write(
     "latihan informatika bersama pak Hendri."
 )
 st.image("IMG_4658.jpeg", width=200)
+
+
+
+st.title("Aplikasi Sederhana")
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
+angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
+
+if (angka % 2) == 0:
+    st.writer(f"{angka} adalah Bilangan Genap")
+else:
+    st.write(f"{angka} adalah Bilangan Ganjil")
